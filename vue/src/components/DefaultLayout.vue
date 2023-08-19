976309f38a514c50c6fc1805806f545710ef31c0
ur7ez <ur7ez@ukr.net>
2023-08-19 18:48:05 +0300

    fix layout data for user

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -31,7 +31,14 @@
                 <div class="flex">
                   <MenuButton class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                    <div class="mx-3">
+                      <div class="text-left text-base font-medium leading-none text-white">
+                        {{ user.name }}
+                      </div>
+                      <div class="text-sm font-medium leading-none text-gray-400">
+                        {{ user.email }}
+                      </div>
+                    </div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8"
                          fill="none" viewBox="0 0 24 24" stroke="white">
                       <path
@@ -85,7 +92,11 @@
         <div class="border-t border-gray-700 pb-3 pt-4">
           <div class="flex items-center px-5">
             <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="white">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
             </div>
             <div class="ml-3">
               <div class="text-base font-medium leading-none text-white">
@@ -97,9 +108,7 @@
             </div>
           </div>
           <div class="mt-3 space-y-1 px-2">
-            <DisclosureButton
-              as="a"
-              @click="logout"
+            <DisclosureButton as="a" @click="logout"
               class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer">
               Sign out
             </DisclosureButton>
@@ -107,7 +116,9 @@
         </div>
       </DisclosurePanel>
     </Disclosure>
-    <router-view/>
+
+    <router-view />
+
   </div>
 </template>
 
