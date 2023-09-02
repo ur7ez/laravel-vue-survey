a0b6e3a744494fc027dd6b665776db225f7fb1ed
ur7ez <ur7ez@ukr.net>
2023-09-02 18:09:05 +0300

    complete onCheckboxChange function for QuestionViewer

--- vue/src/components/viewer/QuestionViewer.vue
+++ vue/src/components/viewer/QuestionViewer.vue
@@ -86,7 +86,13 @@ if (question.type === "checkbox") {
 }
 
 function onCheckboxChange() {
-
+  const selectedOptions = [];
+  for (let text in model.value) {
+    if (model.value[text]) {
+      selectedOptions.push(text);
+    }
+  }
+  emits("update:modelValue", selectedOptions);
 }
 </script>
 
