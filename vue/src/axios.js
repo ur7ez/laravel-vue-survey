048b8f34e613b366506fb8542c428a972fa40729
ur7ez <ur7ez@ukr.net>
2023-09-03 14:43:35 +0300

    add axios interceptors.response

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import store from "./store/index.js";
+import router from "./router";
 
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
@@ -8,5 +9,17 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use(config => {
   config.headers.Authorization = `Bearer ${store.state.user.token}`
   return config;
+})
+
+axiosClient.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 401) {
+    sessionStorage.removeItem('TOKEN')
+    router.push({name: 'Login'})
+  } else if (error.response.status === 404) {
+    router.push({name: 'NotFound'})
+  }
+  throw error;
 })
 export default axiosClient;
